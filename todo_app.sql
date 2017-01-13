d6141c0457a7744286b0822357ce3b2b7aa85ced
Tyler Nichols <tylernchls@gmail.com>
2017-01-12 19:51:35 -1000

    added first task successfully

--- todo_app.sql
+++ todo_app.sql
@@ -12,12 +12,31 @@ CREATE DATABASE todo_app;
 \c todo_app;
 
 CREATE TABLE tasks(
-  id integer primary key,
+  id serial primary key,
   title character varying(255),
   description text,
-  created_at timestamp(no tz),
-  updated_at timestamp(no tz),
+  created_at timestamp,
+  updated_at timestamp,
   completed boolean
 );
+
+ALTER TABLE tasks DROP COLUMN completed;
+
+ALTER TABLE tasks ADD COLUMN completed_at timestamp NULL DEFAULT NULL;
+
+ALTER TABLE tasks ALTER COLUMN updated_at NOT NULL SET DEFAULT now();
+
+INSERT INTO tasks VALUES(DEFAULT, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+
+
+
+
+
+
+
+
+
+
+
 
 \dt;
